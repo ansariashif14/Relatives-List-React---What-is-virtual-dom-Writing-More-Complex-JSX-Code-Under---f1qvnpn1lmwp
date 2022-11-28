90d374514f27ac60a5d5092156b701f80381dc51
ansariashif14 <ansariashif14@gmail.com>
2022-11-28 20:19:52 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,12 @@ class App extends Component {
     render() {
         const arr=["item1", "item2", "item3"];
         const list=arr.map((value)=>{
-            return <li key={`relativeList+${value}`}>{value}</li>
+            return <li key={`relativeList${value}`}>{value}</li>
         })
 
         return(
             <div id="main">
-              <ol>
+              <ol key="relativeList">
                 {list}
               </ol>
             </div>
